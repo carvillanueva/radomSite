3bea070138a0beff3cdac03770ced50c9c4a3b04
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-21 16:11:39 -0400

    hero for mobile

---
+++ sections/Hero.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ArrorIcon from "@/public/assets/arrow-right.svg";
+import cogImage from "@/public/assets/cog.png";
+import Image from "next/image";
+
+export const Hero = () => {
+  return (
+    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+      <div className="container">
+        <div>
+          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+            Version 2.0 is here
+          </div>
+          <h1 className="text-5xl font-bold tracking-tigher bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+            Pathway to productivity
+          </h1>
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, moticate your efforts, and celebrate your success.
+          </p>
+          <div className="flex gap-1 items-center mt-[30px]">
+            <button className="btn btn-primary">Get for free</button>
+            <button className="btn btn-text gap-1">
+              <span>Learn more</span>
+              <ArrorIcon className="h-5 w-5" />
+            </button>
+          </div>
+        </div>
+        <div className="mt-20">
+          <Image src={cogImage} alt="Cog" />
+        </div>
+      </div>
+    </section>
+  );
+};
