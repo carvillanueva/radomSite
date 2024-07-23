90705741f7ab23e8ca39b51fdc8961cf63edbc08
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-23 19:18:59 -0400

    product showcase mobile view

---
+++ sections/ProductShowcase.tsx
@@ -0,0 +1,20 @@
+import productImage from "@public/assets/product-image.png";
+import Image from "next/image";
+
+export const ProductShowcase = () => {
+  return (
+    <section className="bg-gradient-to-b from-[#FFF] to-[#D2DCFF] py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="tag">Boost your productivity</div>
+        </div>
+        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">A more effective way to track your progress</h2>
+        <p className="text-center">
+          Effortlessly turn your ideas into a fully functional, responsive, Saas
+          website in just minutes with this template.
+        </p>
+        <Image src={productImage} alt="Product Image" className="" />
+      </div>
+    </section>
+  );
+};
