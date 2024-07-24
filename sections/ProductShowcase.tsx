0f03906cc4f3f4d3479c1ff41d538bedef93d782
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-23 20:44:11 -0400

    update product for tablet and desktop

--- sections/ProductShowcase.tsx
+++ sections/ProductShowcase.tsx
@@ -1,19 +1,40 @@
 import productImage from "@/public/assets/product-image.png";
+import pyramidImage from "@/public/assets/pyramid.png";
+import tubeImage from "@/public/assets/tube.png";
 import Image from "next/image";
 
 export const ProductShowcase = () => {
   return (
-    <section className="bg-gradient-to-b from-[#FFF] to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-[#FFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="flex justify-center">
-          <div className="tag">Boost your productivity</div>
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            A more effective way to track your progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive,
+            Saas website in just minutes with this template.
+          </p>
         </div>
-        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">A more effective way to track your progress</h2>
-        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
-          Effortlessly turn your ideas into a fully functional, responsive, Saas
-          website in just minutes with this template.
-        </p>
-        <Image src={productImage} alt="Product Image" className="mt-10" />
+        <div className="relative">
+          <Image src={productImage} alt="Product Image" className="mt-10" />
+          <Image
+            src={pyramidImage}
+            alt="Pyramid Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+          <Image
+            src={tubeImage}
+            alt="Tube Image"
+            height={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+          />
+        </div>
       </div>
     </section>
   );
