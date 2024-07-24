b93218d2629228705124b7eeb642c9bccbb6eb63
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-24 19:20:17 -0400

    inital card pricing layout moble

--- sections/Pricing.tsx
+++ sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from '@/public/assets/check.svg'
+
 const pricingTiers = [
   {
     title: "Free",
@@ -52,11 +54,42 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
         <h2 className="section-title">Pricing</h2>
-        <p className="section-description">Free forever. Upgrade for unlimited tasks, better security, and exlusive</p>
-
+        <p className="section-description mt-5">
+          Free forever. Upgrade for unlimited tasks, better security, and
+          exlusive
+        </p>
+        <div>
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              <div className='p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA]'>
+                <h3 className='text-lg font-bold text-black/50'>{title}</h3>
+                <div className='flex items-baseline gap-1 mt-[30px]'>
+                  <span className='text-4xl font-bold tracking-tighter leading-none'>${monthlyPrice}</span>
+                  <span className='tracking-tight font-bold text-black/50'>/month</span>
+                </div>
+                <button className='btn btn-primary w-full mt-[30px]'>{buttonText}</button>
+                <ul className='flex flex-col gap-5 mt-8'>
+                  {features.map((feature) => (
+                    <li className='text-sm flex items-center gap-4'>
+                        <CheckIcon className="h-6 w-6" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </section>
   );
