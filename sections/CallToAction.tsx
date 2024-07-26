8ec3d787f0bcc1c081e39d5db159e567896a1474
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-26 15:36:41 -0400

    update CTA for tablet and desktop

--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,17 +1,37 @@
-import React from "react";
-import ArrowRight from "@/public/assets/arrow-right.svg"
+import ArrowRight from "@/public/assets/arrow-right.svg";
+import starImage from "@/public/assets/star.png";
+import springImage from "@/public/assets/spring.png";
+import Image from "next/image";
+
 export const CallToAction = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <h2 className="section-title">Sign up for free today</h2>
-        <p className="section-description mt-5">Celebrate the jpy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the jpy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+          <Image
+            src={starImage}
+            alt="Star Image"
+            width={360}
+            className="absolute -left-[350px] -top-[137px]"
+          />
+          <Image
+            src={springImage}
+            alt="Spring Image"
+            width={360}
+            className="absolute -right-[331px] -top-[19px]"
+          />
+        </div>
         <div className="flex gap-2 mt-10 justify-center">
-            <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text gap-1">
-                <span>Learn more</span>
-                <ArrowRight className="h-5 w-5" />
-            </button>
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5" />
+          </button>
         </div>
       </div>
     </section>
