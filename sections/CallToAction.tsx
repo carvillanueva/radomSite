5f03340f3cf7188d397ec0d27168ac499cfe6530
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-07-26 15:32:07 -0400

    CTA mobile done

---
+++ sections/CallToAction.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ArrowRight from "@/public/assets/arrow-right.svg"
+export const CallToAction = () => {
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+      <div className="container">
+        <h2 className="section-title">Sign up for free today</h2>
+        <p className="section-description mt-5">Celebrate the jpy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
+        <div className="flex gap-2 mt-10 justify-center">
+            <button className="btn btn-primary">Get for free</button>
+            <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowRight className="h-5 w-5" />
+            </button>
+        </div>
+      </div>
+    </section>
+  );
+};
--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -83,22 +83,24 @@ const TestimonalsColumn = (props: {
     )}
   >
     {props.testimonials.map(({ text, imageSrc, name, username }) => (
-      <div className="card">
-        <div>{text}</div>
-        <div className="flex items-center gap-2 mt-5">
-          <Image
-            src={imageSrc}
-            alt={name}
-            width={40}
-            height={40}
-            className="h-10 w-10 rounded-full"
-          />
-          <div className="">
-            <div className="font-medium tracking-tight leading-5">{name}</div>
-            <div className="leading-5 tracking-tight">{username}</div>
+      <>
+        <div className="card">
+          <div>{text}</div>
+          <div className="flex items-center gap-2 mt-5">
+            <Image
+              src={imageSrc}
+              alt={name}
+              width={40}
+              height={40}
+              className="h-10 w-10 rounded-full"
+            />
+            <div className="">
+              <div className="font-medium tracking-tight leading-5">{name}</div>
+              <div className="leading-5 tracking-tight">{username}</div>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     ))}
   </div>
 );
